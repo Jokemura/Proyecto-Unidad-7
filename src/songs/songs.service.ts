6825a01837d8b1b295d6896da1c9d8d7e10aba5f
Jokemura <angelfv1235@gmail.com>
2023-01-24 23:04:09 -0500

    changing loging to Hmac

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,17 +1,9 @@
 import { db } from "../database/db.server";
+import { Song } from "@prisma/client";
 
-type song = {
-    id: number;
-    name: string;
-    artist: string;
-    album: string;
-    year: string | null ;
-    genre: string;
-    duration: number;
-    isPublic: boolean
-}
 
-export const listSongs = async (): Promise<song[]> => {
+
+export const listSongs = async (): Promise<Song[]> => {
     return db.song.findMany({
         select: {
             id: true,
@@ -29,7 +21,7 @@ export const listSongs = async (): Promise<song[]> => {
 
 //listar canciones por id
 
-export const getSongs = async (id: number): Promise<song | null> => {
+export const getSongs = async (id: number): Promise<Song | null> => {
     return db.song.findUnique({
         where: {
             id,
@@ -50,7 +42,7 @@ export const getSongs = async (id: number): Promise<song | null> => {
 
 
 // Funcion para crear nuevas canciones
-export const createSong = async (song: Omit<song, 'id'>): Promise<song> => {
+export const createSong = async (song: Omit<Song, 'id'>): Promise<Song> => {
     const { name, artist, album, year, genre, duration, isPublic} = song;
     return db.song.create({
         data: {
@@ -76,7 +68,7 @@ export const createSong = async (song: Omit<song, 'id'>): Promise<song> => {
 }
 
 //Funcion para listar por id
-export const getUser = async (id: number): Promise< song | null> => {
+export const getUser = async (id: number): Promise< Song | null> => {
     return db.song.findUnique({
         where: {
             id,
@@ -97,7 +89,7 @@ export const getUser = async (id: number): Promise< song | null> => {
 
 
 // Funcion para Actualizar canciones
-export const updateSong = async (song: Omit<song, 'id'>, id:number): Promise<song> => {
+export const updateSong = async (song: Omit<Song, 'id'>, id:number): Promise<Song> => {
     const { name, artist, album, year, genre, duration, isPublic } = song;
     return db.song.update({
         where: {
--- src/users/service.ts
+++ src/users/service.ts
@@ -1,17 +1,72 @@
 import { db } from "../database/db.server"
+import { User } from "@prisma/client"
+import { createHmac } from "node:crypto"
 
-type user = {
-    id: number
-    email: string
-    name: String
-}
 
-export const Userslist = async (): Promise<user[]> => {
+
+export const listUsers = async (): Promise<User[]> => {
     return db.user.findMany({
         select: {
             id: true,
             name: true,
             email: true,
+            password: true,
+            last_session: true,
+            updated_at: true,
+            date_born: true,
         }
     })
+}
+
+
+
+export const createUser = async (user: Omit<User, 'id'>): Promise<User> => {
+    const { email, name, password } = user;
+    if(password === null) {
+        throw new Error("La contraseña no puede ser nula");
+    }
+    const hash = createHmac("sha256", "salt-key").update(password).digest("hex");
+    return db.user.create({
+        data: {
+            email,
+            name,
+            password: hash,
+            last_session: new Date,
+            date_born : new Date,
+            
+                        
+        },
+        select: {
+            id: true,
+            email: true,
+            name: true,
+            password: true,
+            last_session: true,
+            date_born: true,
+            updated_at: true
+    
+    }    })
+}
+
+export const getUser = async (id: number): Promise<User | null> => {
+    const user = await db.user.findUnique({
+        where: {
+            id,
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            password: true,
+            last_session: true,
+            updated_at: true,
+            date_born: true
+        }
+    });
+    if(!user) return null;
+    
+    
+    user.updated_at = new Date();
+    user.date_born = new Date();
+    return user;
 }
